ec579f9cf7dd47de13bbd62187cce02ab6996017
Your Name <you@example.com>
2022-01-09 17:11:54 +0300

    added

---
+++ files/js/custom.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $(document).on('click', '.close-coup', function() {
+    $('.bottom-coup').addClass('relative');
+  });
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() >= 500) {
+      $('.bottom-coup').addClass('sticky');
+    }
+  });
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+      $('.bottom-coup').removeClass('sticky');
+    }
+  });
+});
